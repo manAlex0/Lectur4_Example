b65dfac577f0fcdc6438bfc60f7a920a2ef2d950
Aleksandr Khan <manchig1990@gmail.com>
2024-03-22 19:01:33 +1000

    add Task's

--- Program.cs
+++ Program.cs
@@ -1,2 +1,188 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//***********************************************Example001_RecursionAlgorithm
+
+string[,] table = new string[2, 5];
+// String.Empty
+// table[0, 0] table[0, 1] table[0, 2] table[0, 3] table[0, 4]
+// table[1, 0] table[1, 1] table[1, 2] table[1, 3] table[1, 4]
+
+table[1, 2] = "слово";
+for (var rows = 0; rows < 2; rows++)
+{
+    for (var columns = 0; columns < 5; columns++)
+    {
+        System.Console.WriteLine($"-{table[rows, columns]}-");
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (var i = 0; i < matr.GetLength(0); i++)
+    {
+        for (var j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (var i = 0; i < matr.GetLength(0); i++)
+    {
+        for (var j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10); // [1;10)
+        }
+    }
+}
+
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+System.Console.WriteLine();
+PrintArray(matrix);
+
+
+
+int[,] pic = new int[,]
+{
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+ {0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+};
+
+void PrintImage(int[,] image)
+{
+    for (var i = 0; i < image.GetLength(0); i++)
+    {
+        for (var j = 0; j < image.GetLength(1); j++)
+        {
+
+            if (image[i, j] == 0) System.Console.Write($" ");
+            else System.Console.Write($"+");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FillImage(int row, int col)
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row, col + 1);
+    }
+}
+
+
+PrintImage(pic);
+FillImage(13, 13);
+PrintImage(pic);
+
+//**********************************
+
+// int Factorial(int n)
+// {
+//  // 1! = 1
+//  // 0! = 1
+//  if(n == 1) return 1;
+//  else return n * Factorial(n-1);
+// }
+// Console.WriteLine(Factorial(3)); // 1 * 2 * 3 = 6
+
+// int Factorial(int n)
+// {
+//  // 1! = 1
+//  // 0! = 1
+//  if(n == 1) return 1;
+//  else return n * Factorial(n-1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//  Console.WriteLine(Factorial(i));
+// }
+
+// int Factorial(int n)
+// {
+//     // 1! = 1
+//     // 0! = 1
+//     if (n == 1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+// double Factorial(int n)
+// {
+//  // 1! = 1
+//  // 0! = 1
+//  if(n == 1) return 1;
+//  else return n * Factorial(n-1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//  Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+// int Fibonacci(int n)
+// {
+//     if (n == 1 || n == 2) return 1;
+//     else return Fibonacci(n - 1) + Fibonacci(n - 2);
+// }
+
+// for (int i = 1; i < 10; i++)
+// {
+//     Console.WriteLine(Fibonacci(i));
+// }
+
+
+// double Fibonacci(int n)
+// {
+//  if(n == 1 || n == 2) return 1;
+//  else return Fibonacci(n-1) + Fibonacci(n-2);
+// }
+// for (int i = 1; i < 50; i++)
+// {
+//  Console.WriteLine(Fibonacci(i));
+// }
+
+double Fibonacci(int n)
+{
+ if(n == 1 || n == 2) return 1;
+ else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 50; i++)
+{
+ Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
